48d98f242ce45592ed8d764183ec9ab1acb5c0de
haeun104 <haeun104@gmail.com>
2024-07-04 19:06:00 +0200

    Connect mongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// enable to import data from env file
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
