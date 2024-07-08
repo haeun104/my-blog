c82db348f796e2ebd5e03a102f3c5d3f1a7bb9e3
haeun104 <haeun104@gmail.com>
2024-07-08 18:07:29 +0200

    Create footer section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,10 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
-import Projects from "./pages/Projects";
+import BlogPosts from "./pages/BlogPosts";
 import SignIn from "./pages/SignIn";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -17,8 +18,9 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/projects" element={<Projects />} />
+        <Route path="/blog-posts" element={<BlogPosts />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,89 @@
+import { Footer } from "flowbite-react";
+import { BsFacebook, BsInstagram, BsTwitterX } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+const FooterComp = () => {
+  return (
+    <Footer container className="border border-t-8 border-gray-200">
+      <div className="w-full">
+        <div className="w-full lg:flex justify-between mx-auto max-w-7xl">
+          <div className="py-4">
+            <Link
+              to="/"
+              className="whitespace-nowrap font-semibold dark:text-white text-lg sm:text-xl"
+            >
+              <span className="px-2 py-1 text-white rounded-md pinkToOrange-gradient">
+                MY
+              </span>{" "}
+              BLOG
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:gap-8 mt-4 pb-4">
+            <div>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="/about">Introduction</Footer.Link>
+                <Footer.Link href="/about">Contact</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Blog posts" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="/blog-posts"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Culture
+                </Footer.Link>
+                <Footer.Link
+                  href="/blog-posts"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Food
+                </Footer.Link>
+                <Footer.Link
+                  href="/blog-posts"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Shopping
+                </Footer.Link>
+                <Footer.Link
+                  href="/blog-posts"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Event
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms & Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="My blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-4 mt-2 sm:mt-0 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsTwitterX} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+};
+
+export default FooterComp;
