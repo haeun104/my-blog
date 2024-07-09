7160e2cac938207ec0f45553f9fb7fcb0324d42c
haeun104 <haeun104@gmail.com>
2024-07-09 18:56:20 +0200

    Set up redux persist to keep user data

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,17 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { store } from "./redux/store.ts";
+import { persist, store } from "./redux/store.ts";
 import { Provider } from "react-redux";
 import React from "react";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
+  <PersistGate persistor={persist}>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </PersistGate>
 );
