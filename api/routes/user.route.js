7e10653af44f6e38695c01a45e40624847a89685
haeun104 <haeun104@gmail.com>
2024-07-12 19:02:06 +0200

    Create API for deleting user

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { updateUserProfile } from "../controllers/user.controller.js";
+import {
+  deleteUser,
+  updateUserProfile,
+} from "../controllers/user.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.put("/update/:userId", verifyUser, updateUserProfile);
+router.delete("/delete/:userId", verifyUser, deleteUser);
 
 export default router;
