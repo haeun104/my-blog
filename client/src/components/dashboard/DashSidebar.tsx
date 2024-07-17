d7106bd40d44f7e314a76096a45fa54a9fee2b4f
haeun104 <haeun104@gmail.com>
2024-07-17 16:21:29 +0200

    Add posts on dashboard sidebar

--- client/src/components/dashboard/DashSidebar.tsx
+++ client/src/components/dashboard/DashSidebar.tsx
@@ -1,9 +1,10 @@
 import { Sidebar } from "flowbite-react";
 import { useEffect, useState } from "react";
-import { HiArrowSmLeft, HiUser } from "react-icons/hi";
-import { useDispatch } from "react-redux";
+import { HiArrowSmLeft, HiDocumentText, HiUser } from "react-icons/hi";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import { signoutSuccess } from "../../redux/user/userSlice";
+import { RootState } from "../../redux/store";
 
 const DashSidebar = () => {
   const location = useLocation();
@@ -11,6 +12,7 @@ const DashSidebar = () => {
   const [tab, setTab] = useState<string | undefined>();
 
   const dispatch = useDispatch();
+  const { currentUser } = useSelector((state: RootState) => state.user);
 
   useEffect(() => {
     const query = new URLSearchParams(location.search);
@@ -42,18 +44,29 @@ const DashSidebar = () => {
   return (
     <Sidebar className="w-full">
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className="flex flex-col gap-1">
           <Link to="/dashboard?tab=profile">
             <Sidebar.Item
               icon={HiUser}
               active={tab === "profile"}
-              label={"User"}
+              label={currentUser?.isAdmin ? "Admin" : "User"}
               labelColor="dark"
               as="div"
             >
               Profile
             </Sidebar.Item>
           </Link>
+          {currentUser?.isAdmin && (
+            <Link to="/dashboard?tab=posts">
+              <Sidebar.Item
+                icon={HiDocumentText}
+                active={tab === "posts"}
+                as="div"
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
+          )}
           <Sidebar.Item
             icon={HiArrowSmLeft}
             onClick={handleSignout}
