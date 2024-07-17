5091176195effed9fe4f5ef58533cd6d7de9811c
haeun104 <haeun104@gmail.com>
2024-07-17 21:12:45 +0200

    Add show more functionality

--- client/src/components/dashboard/DashPosts.tsx
+++ client/src/components/dashboard/DashPosts.tsx
@@ -18,6 +18,7 @@ interface Post {
 
 const DashPosts = () => {
   const [posts, setPosts] = useState<Post[]>([]);
+  const [showMore, setShowMore] = useState(true);
 
   const { currentUser } = useSelector((state: RootState) => state.user);
 
@@ -31,6 +32,9 @@ const DashPosts = () => {
 
         if (response.ok) {
           setPosts(data.posts);
+          if (data.total <= 9) {
+            setShowMore(false);
+          }
         }
       } catch (error) {
         // @ts-expect-error error type is any
@@ -42,58 +46,88 @@ const DashPosts = () => {
     }
   }, [currentUser]);
 
+  const handleShowMore = async () => {
+    try {
+      const response = await fetch(
+        `api/post/getPosts?userId=${currentUser?._id}&startIndex=${posts.length}`
+      );
+      const data = await response.json();
+      if (response.ok) {
+        setPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      // @ts-expect-error error type is any
+      console.log(error.message);
+    }
+  };
+
   return (
-    <div className="mt-5 overflow-x-auto md:mx-auto px-3 table-auto scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
+    <div className="my-5 overflow-x-auto md:mx-auto px-3 table-auto scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       {currentUser?.isAdmin && posts.length > 0 ? (
-        <Table className="shadow-md">
-          <Table.Head className="border-b-[1px]">
-            <Table.HeadCell>Update date</Table.HeadCell>
-            <Table.HeadCell>Post image</Table.HeadCell>
-            <Table.HeadCell>Post title</Table.HeadCell>
-            <Table.HeadCell>Category</Table.HeadCell>
-            <Table.HeadCell>Delete</Table.HeadCell>
-            <Table.HeadCell>
-              <span>Edit</span>
-            </Table.HeadCell>
-          </Table.Head>
-          <Table.Body>
-            {posts.map((post) => (
-              <Table.Row
-                key={post._id}
-                className="border-b-[1px]  bg-white dark:border-gray-700 dark:bg-gray-800"
-              >
-                <Table.Cell>
-                  {new Date(post.updatedAt).toLocaleDateString()}
-                </Table.Cell>
-                <Table.Cell>
-                  {post.images ? (
-                    <img
-                      src={post.images[0]}
-                      className="w-16 h-12 object-cover"
-                    />
-                  ) : (
-                    <span>No image</span>
-                  )}
-                </Table.Cell>
-                <Table.Cell className="font-semibold">{post.title}</Table.Cell>
-                <Table.Cell>{post.category}</Table.Cell>
-                <Table.Cell>
-                  <span className="cursor-pointer text-rose-500 hover:underline">
-                    Delete
-                  </span>
-                </Table.Cell>
-                <Table.Cell>
-                  <Link
-                    to={`/update-post/${post._id}`}
-                    className="text-teal-500 hover:underline"
-                  >
-                    Edit
-                  </Link>
-                </Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
+        <>
+          <Table className="shadow-md">
+            <Table.Head className="border-b-[1px]">
+              <Table.HeadCell>Update date</Table.HeadCell>
+              <Table.HeadCell>Post image</Table.HeadCell>
+              <Table.HeadCell>Post title</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>
+                <span>Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+            <Table.Body>
+              {posts.map((post) => (
+                <Table.Row
+                  key={post._id}
+                  className="border-b-[1px]  bg-white dark:border-gray-700 dark:bg-gray-800"
+                >
+                  <Table.Cell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {post.images ? (
+                      <img
+                        src={post.images[0]}
+                        className="w-16 h-12 object-cover"
+                      />
+                    ) : (
+                      <span>No image</span>
+                    )}
+                  </Table.Cell>
+                  <Table.Cell className="font-semibold">
+                    {post.title}
+                  </Table.Cell>
+                  <Table.Cell>{post.category}</Table.Cell>
+                  <Table.Cell>
+                    <span className="cursor-pointer text-rose-500 hover:underline">
+                      Delete
+                    </span>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      to={`/update-post/${post._id}`}
+                      className="text-teal-500 hover:underline"
+                    >
+                      Edit
+                    </Link>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+          {showMore && (
+            <button
+              className="text-teal-500 w-full py-2 border-teal-400 border-[1px] rounded-sm shadow-md text-sm mt-5 hover:bg-teal-500 hover:text-white"
+              onClick={handleShowMore}
+            >
+              Show more
+            </button>
+          )}
+        </>
       ) : (
         <p>There are no posts yet</p>
       )}
